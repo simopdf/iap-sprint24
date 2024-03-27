0a3f926f1d59eac6402bd32369a0cd967715afab
AelisR <159459452+AelisR@users.noreply.github.com>
2024-03-27 11:39:20 +0100

    Update Projections.py
    ajout de commentaires

--- Projections.py
+++ Projections.py
@@ -3,6 +3,9 @@ import numpy as np
 
 #valeur défault de f:
 F = 90
+##Dans ce fichier on crée les 3 classes que nous allons utiliser dans le reste du code 
+#Tout d'abord la classe 'point' pour manipuler ds points en 2D ou 3D, pour faciliter tous nos changements de coordonnées
+#attention : la classe 'Point' existe déjà dans python mais ses attributs ne correspondent pas à nos besoins alors on en crée une nouvelle selon les caractéristiques que nous voulons
 
 
 class point:
@@ -27,24 +30,28 @@ class point:
 
     
 #################################################
-
+#Ensuite la classe 'f', qui hérite des attributs de 'point'.
 
 class f(point):
+    #initialisation de la classe
     def __init__(self,f0=F):
         self.f=f0
 
+    #formatage de l'affichage lors d'un print de notre objet
     def __repr__(self):
         return f"f = {self.f} °" 
     
 
-
+    #fonctions de changement de changement de référentiel
     def to_width(self):   #donne les coordonnées de G et D en sachant l'angle f
+        '''demi-angle -> coordonnées des bords de l'écran sur les x'''
         f0_rad = np.radians(self.f)
-        return [point(-np.sin(f0_rad/2),0,np.cos(f0_rad/2)).stereo(), point(np.sin(f0_rad/2),0,np.cos(f0_rad/2)).stereo()]
+        return [point(-np.sin(f0_rad/2),0,np.cos(f0_rad/2)).stereo(), point(np.sin(f0_rad/2),0,np.cos(f0_rad/2)).stereo()]   #formule démontrée (voir l'annexe)
 
     
     def to_angle(self,G,D):
-        l = (np.abs(G.invstereo().x)) + np.abs((D.invstereo().x))
+        '''coordonnées des bords de l'écran sur les x (2 points) -> demi-angle f'''
+        l = (np.abs(G.invstereo().x)) + np.abs((D.invstereo().x))   #formule démontrée (voir l'annexe)
         return f(360 - 2*np.degrees(np.arcsin(0.5*l)))
     
     def L(self):  #donne la largeur de l'ecran 
@@ -59,6 +66,7 @@ def screen(self, resX, resY):
     x,y,_ = self.stereo().vars()
     L = f().L()
 
+    # relations démontrées (voir l'annexe)
     X = (2*x/L)*resX + resX/2
     Y = (2*y/L)*resX + resY/2
     return point(X,Y)
@@ -70,12 +78,14 @@ def invscreen(self,resX,resY):
     X = self.x
     Y = self.y
 
+    # relations démontrées (voir l'annexe)
     x = (X - resX/2)*L/(2*resX)
     y = (Y - resY/2)*L/(2*resX)
 
     return point(x,y)
 
 
+#Ajout des méthodes 'screen' et 'invscreen' à la classe 'point'. On les ajoute de cette manière apres la définition de la classe car elles utilisent des propriétés de la classe 'f', qui doit être définie après la classe 'point'.
 point.screen = screen # add methods screen and invscreen to point. methods employ f class
 point.invscreen = invscreen
 
@@ -84,9 +94,12 @@ point.invscreen = invscreen
 
 
 #################################################
-    
+#Ensuite la classe 'Rotation', qui hérite des attributs de 'point'. Elle implémente les rotations de la caméra afin de permettre une vue à 360° de la sphère céleste.
+#Les matrices de rotation utilisées ainsi que leur combinaison est démontré (voir l'annexe)
+
 class Rotation(point):
 
+    #Initialiation de la classe
     def __init__(self, psi = 0, theta = 0, phi = 0):
         self.psi= np.radians(psi)
         self.theta = np.radians(theta)
