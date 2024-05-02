83d0c96b453edb75f530a119a643760c41b0b308
simopdf <118457952+simopdf@users.noreply.github.com>
2024-05-02 11:39:58 +0200

    v2 videogen.py

--- videogen.py
+++ videogen.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 
+fps = 30 #nb d'images par seconde
+
 image_folder = "C:\\Users\\Utente\\Documents\\Stage IAP\\video 1min v1"
 
 images = [img for img in os.listdir(image_folder) 
@@ -8,7 +10,8 @@ images = [img for img in os.listdir(image_folder)
 
 
 frame = cv2.imread(os.path.join(image_folder, images[0])) 
-video = cv2.VideoWriter("video 1.avi", 0, 30, (640,480)) 
+height, width, layers = frame.shape
+video = cv2.VideoWriter("video 1.avi", 0, fps, (width,height)) 
 
 for i,image in enumerate(images):  
         video.write(cv2.imread(os.path.join(image_folder, image)))  
