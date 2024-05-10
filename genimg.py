93f355e6e35dc70ad3c374cd7a5e0df3736c61ed
fug4r <166860013+fug4r@users.noreply.github.com>
2024-05-10 15:37:21 +0000

    Projection equirectangulaire ?

--- genimg.py
+++ genimg.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 import os
-from PIL import Image #optionnel, utile que pour sauvegarder les images individuellement
+from PIL import Image
 import cv2
 
 
@@ -34,6 +34,19 @@ def plane_to_sphere(x, y):
     return x_n, y_n, z_n
 
 
+# Projection equirectangulaire (compatible VR)
+def sphere_to_rectangle(r, theta, phi):
+    x_r = r * phi
+    y_r = r * (np.pi / 2 - theta)
+    return x_r, y_r
+
+
+def rectangle_to_sphere(r, x_r, y_r):
+    theta = y_r / r - np.pi / 2
+    phi = x_r / r
+    return theta, phi
+
+
 # Angle <-> Longueur
 def f_to_length(angle_degrees):
     angle_radians = np.radians((180 - angle_degrees) / 2)
@@ -48,13 +61,6 @@ def length_to_f(length):
     return 180 - 2 * angle_degrees
 
 
-# Taille de l'ecran
-def screen_size(resX, resY, length):
-    ratio = resX / resY
-    width = length / ratio
-    return length, width
-
-
 # Ecran <-> Plan
 def plane_to_screen(resX, resY, x, y, f):
     L = f_to_length(f)
@@ -65,6 +71,21 @@ def plane_to_screen(resX, resY, x, y, f):
 
 def screen_to_plane(resX, resY, X, Y, f):
     L = f_to_length(f)
+    x = (X - resX / 2) * L / (2 * resX)
+    y = (Y - resY / 2) * L / (2 * resX)
+    return x, y
+
+
+# Ecran <-> Rectangle
+def rectange_to_screen(resX, resY, x, y, r):
+    L = r * np.pi
+    X = (2 * x / L) * resX + resX / 2
+    Y = (2 * y / L) * resX + resY / 2
+    return X, Y
+
+
+def screen_to_rectangle(resX, resY, X, Y, r):
+    L = r * np.pi
     x = (X - resX / 2) * L / (2 * resX)
     y = (Y - resY / 2) * L / (2 * resX)
     return x, y
@@ -141,99 +162,123 @@ def lorentz(n_x, n_y, n_z, d_theta, d_phi, v):
 
     return *k[1:], wp
 
-def modulation_couleur(C,w):      #C = couleur, w = omega de lorentz ----- la fonction est 100% vectorisée
-    # Transfo sRGB -> lin
-    C_lin = np.where(C/255 <= 0.04045, (C/255)/12.92, ((C/255 + 0.055)/1.055) ** 2.4)
 
-    # Transfo fréquence (1/w)
+# Transformation sRGB -> RGB
+def modulation_couleur(C, w):
+    C_lin = np.where(
+        C / 255 <= 0.04045, (C / 255) / 12.92, ((C / 255 + 0.055) / 1.055) ** 2.4
+    )
+
+    # Transformation frequence (1/w)
     C_lin = C_lin / w
 
-    # Transfo lin -> sRGB
-    C_nouveau = np.where(C_lin <= 0.0031308, 12.92 * C_lin, 1.055 * ( C_lin ** (1/2.4)) - 0.055)
-   
+    # Transformation RGB -> sRGB
+    C_nouveau = np.where(
+        C_lin <= 0.0031308, 12.92 * C_lin, 1.055 * (C_lin ** (1 / 2.4)) - 0.055
+    )
+
     return np.minimum((C_nouveau * 255).astype(int), 255)
+
 
 # Couleur associee a chaque direction
 def point_to_colour(resX, resY, theta, phi, w):
-    
-    dark = np.array([245,66,93])
-    light = np.array([200, 200, 200])
-    res = np.zeros([resY, resX , 3])  # array 3D: pour chaque couple de coords (= un pixel) on associe un array de dim (1x3) qui corréspond aux val RGB des couleurs
+    # Coleurs des cadrillage
+    if COLOR:
+        dark = np.array([240, 70, 100])
+        light = np.array([180, 180, 180])
+    else:
+        dark = np.array([80, 80, 80])
+        light = np.array([130, 130, 130])
 
+    # array 3D: pour chaque couple de coords (= un pixel) on associe un array de dim (1x3) qui correspond aux val RGB des couleurs
+    res = np.zeros([resY, resX, 3])
 
-    mask = (theta // 5 + phi // 5) % 2 #on crée le mask corréspondant à la disposition des carrés
-    mask = np.repeat(mask[:,:,np.newaxis], 3, axis=2) #on extend le mask à la 3eme dimension
-    w = np.repeat(w[:,:,np.newaxis],3,axis=2) #on extend le tableau w (dim=resY,resX) pour qu'il devienne de dim ResY,ResX,3
+    # on cree le mask correspondant a la disposition des carres
+    mask = (theta // 5 + phi // 5) % 2
+    # on extend le mask a la 3eme dimension
+    mask = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
+    # on extend le tableau w (dim=resY,resX) pour qu'il devienne de dim ResY,ResX,3
+    w = np.repeat(w[:, :, np.newaxis], 3, axis=2)
+    # on cree un tableau 3D de couleurs RGB a partir du mask --> on va l'utiliser pour calculer les couleurs modulees
+    color = np.where(mask, dark, light)
 
-    color = np.where(mask, dark, light) # on crée un tableau 3D de couleurs RGB à partir du mask --> on va l'utiliser pour calculer les couleurs modulées
-    
-    res = modulation_couleur(color, w) #couleurs modulées
-    
-           
+    res = modulation_couleur(color, w)  # couleurs modulees
+
     return res
+
 
 # Couleur associee a chaque pixel
 def pixel_to_colour(resX, resY, f, X, Y, psi, theta, phi, v):
     global check
 
-    x, y = screen_to_plane(resX, resY, X, Y, f)
-    x_n, y_n, z_n = plane_to_sphere(x, y)
-    if DEBUG:
-        print(f"Ecran -> Plan -> Sphere 🗸 ({time.time() - check:.2f}s)")
-        check = time.time()
+    # To VR or not to VR
+    if EQUIRECTANGULAR:
+        r = 1  # Sphere unitaire
+        x, y = screen_to_rectangle(resX, resY, X, Y, r)
+        theta_r, phi_r = rectangle_to_sphere(r, x, y)
+        x_n, y_n, z_n = spherical_to_cartesian(r, theta_r, phi_r)
+        if DEBUG:
+            print(f"Ecran -> Rectangle -> Sphere 🗸 ({time.time() - check:.2f}s)")
+            check = time.time()
+
+    else:
+        x, y = screen_to_plane(resX, resY, X, Y, f)
+        x_n, y_n, z_n = plane_to_sphere(x, y)
+        if DEBUG:
+            print(f"Ecran -> Plan -> Sphere 🗸 ({time.time() - check:.2f}s)")
+            check = time.time()
 
     u_n, v_n, w_n = cam_to_sun(x_n, y_n, z_n, psi, theta, phi)
     if DEBUG:
         print(f"Rotations 🗸 ({time.time() - check:.2f}s)")
         check = time.time()
 
-    x_l, y_l, z_l, r = lorentz(u_n, v_n, w_n, 0, 0, v)
+    x_l, y_l, z_l, r = lorentz(u_n, v_n, w_n, *direction, v)
     if DEBUG:
         print(f"Transformations de Lorentz 🗸 ({time.time() - check:.2f}s)")
         check = time.time()
 
     _, theta, phi = cartesian_to_spherical(x_l, y_l, z_l)
-    colour = point_to_colour(resX, resY, np.degrees(theta), np.degrees(phi), r)
+    color = point_to_colour(resX, resY, np.degrees(theta), np.degrees(phi), r)
     if DEBUG:
         print(f"Cartesien -> Spherique -> Couleurs 🗸 ({time.time() - check:.2f})s")
         check = time.time()
 
-    return colour
+    return color
 
 
 # Dessine l'ecran
 def paint(resX, resY, f, psi, theta, phi, v, nom=None):
-
     # Tableaux de coordonnees
     x = np.linspace(1, resX, resX)
     y = np.linspace(1, resY, resY)
     X, Y = np.meshgrid(x, y)
 
     # Tableau de couleurs pour chaque pixel
-    image_array = np.empty([resY, resX, 3])  
+    image_array = np.empty((resY, resX, 3))
     image_array = pixel_to_colour(resX, resY, f, X, Y, psi, theta, phi, v)
-
 
     if DEBUG:
         print(f"Image data 🗸 ({time.time() - check:.2f}s)")
-
-    if DEBUG_frame:
-        img.save(str(nom)+".png") #les images ne sont plus sauvegardées en mémoire. on peut tout de meme les sauvegarder pour le debug
 
     return image_array
 
 
 # Parametres d'execution
-DEBUG = True
-DEBUG_frame = False
-tau_tot = 20  # s
-c = 1  # c
-v_fin = 0.999995  # c
-resX = 640  # pixels
-resY = 480  # pixels
-fps = 30
+DEBUG = False
+DEBUG_FRAME = False
+EQUIRECTANGULAR = True
+COLOR = True
+tau_tot = 10  # S
+c = 1  # C
+v_fin = 0.999995  # C
+orientaion = (0, 0, 0)  # Orientation camera (psi, theta, phi)
+direction = (-np.pi / 2, 0)  # Direction de deplacement (theta, phi)
+resX = 320  # Pixels
+resY = 200  # Pixels
+fps = 30  # Fps
 
-# Execution ----> créer une fonction pour automatiser?
+# Execution
 start_time = time.time()
 
 n = tau_tot * fps
@@ -241,21 +286,37 @@ tau = np.linspace(0, tau_tot, n)
 
 a = (c / tau_tot) * np.arctanh(v_fin / c)
 v = c * np.tanh(a / c * tau)
-video = cv2.VideoWriter("video test 3.avi", 0, fps=fps, frameSize= (resX,resY))  #ici on crée un objet vidéo de OpenCV
+
+# Cree un objet video de OpenCV
+video = cv2.VideoWriter("film.avi", 0, fps, (resX, resY))
 for i in range(n):
     check_loop = time.time()
     check = time.time()
-    img = paint(resX, resY, 90, 0, 0, 0, v[i]) #ici on crée à chaque itération le np.array de pixels
-    
-    video.write(np.uint8(img)) #on utilise le array pour créer un frame
-    
 
+    # Cree a chaque iteration le np.array de pixels
+    img = paint(resX, resY, 90, *orientaion, v[i], i)
+
+    # Array -> Frame
+    video.write(np.uint8(img))
+
+    # Messages debug
     if DEBUG:
         print(f"Image {i+1} 🗸 ({time.time() - check_loop:.2f}s)")
+        print(f"Progress: {(i+1)/n*100:6.2f}%")
         print("\n")
-    print(f"Progress: {(i+1)/n*100:6.2f}%")
+    else:
+        os.system("clear")  # "cls" sous Windows
+        print(f"Progress: {(i+1)/n*100:6.2f}%")
+
+    # Les images ne sont plus sauvegardees en memoire, on peut tout de meme les sauvegarder pour le debug
+    if DEBUG_FRAME:
+        # RGB -> BRG, car c'est le format de Image.fromarray()
+        img[:, :, [0, 2]] = img[:, :, [2, 0]]
+        image = Image.fromarray(np.uint8(img))
+        image.save(str(i) + ".png")
+
 
 end_time = time.time()
 
 print(f"\nTemps d'excution moyen: {(end_time - start_time)/n:.2f}s")
-print(f"\nTemps d'excution total: {(end_time - start_time):.2f}s")
+print(f"Temps d'excution total: {(end_time - start_time):.2f}s")
